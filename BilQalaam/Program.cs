499dc2164f97130d2444c65f5d24ffa724d2f9f8
Abdelaziz Zahran <zizoelbeltagy266@gmail.com>
2025-12-21 16:16:11 +0200

    update

--- BilQalaam/Program.cs
+++ BilQalaam/Program.cs
@@ -10,8 +10,6 @@ using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-// 🔥 Railway PORT binding (مهم جدًا)
-builder.WebHost.UseUrls($"http://0.0.0.0:{Environment.GetEnvironmentVariable("PORT") ?? "8080"}");
 
 // =====================
 // Infrastructure
@@ -19,20 +17,11 @@ builder.WebHost.UseUrls($"http://0.0.0.0:{Environment.GetEnvironmentVariable("PO
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(UserProfile).Assembly);
 
-//builder.Services.AddDbContext<BilQalaamDbContext>(options =>
-//    options.UseSqlServer(
-//        builder.Configuration.GetConnectionString("DefaultConnection")
-//    )
-//);
 builder.Services.AddDbContext<BilQalaamDbContext>(options =>
-{
-    var connectionString = builder.Configuration["DATABASE_URL"];
-
-    if (string.IsNullOrEmpty(connectionString))
-        throw new Exception("DATABASE_URL is missing");
-
-    options.UseNpgsql(connectionString);
-});
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 
 // =====================
 // Identity
@@ -137,52 +126,54 @@ var app = builder.Build();
 // =====================
 // Auto Apply Migrations + Seed
 // =====================
-//using (var scope = app.Services.CreateScope())
-//{
-//    var dbContext = scope.ServiceProvider.GetRequiredService<BilQalaamDbContext>();
-//    dbContext.Database.Migrate();
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<BilQalaamDbContext>();
+    dbContext.Database.Migrate();
 
-//    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-//    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
-//    const string superAdminEmail = "superadmin@bilqalaam.com";
-//    const string superAdminPassword = "Aa@12345#";
-//    const string superAdminRole = "SuperAdmin";
+    const string superAdminEmail = "superadmin@bilqalaam.com";
+    const string superAdminPassword = "Aa@12345#";
+    const string superAdminRole = "SuperAdmin";
 
-//    if (!await roleManager.RoleExistsAsync(superAdminRole))
-//    {
-//        await roleManager.CreateAsync(new IdentityRole(superAdminRole));
-//    }
+    if (!await roleManager.RoleExistsAsync(superAdminRole))
+    {
+        await roleManager.CreateAsync(new IdentityRole(superAdminRole));
+    }
 
-//    var superAdminUser = await userManager.FindByEmailAsync(superAdminEmail);
-//    if (superAdminUser == null)
-//    {
-//        superAdminUser = new ApplicationUser
-//        {
-//            UserName = superAdminEmail,
-//            Email = superAdminEmail,
-//            FullName = "Super Admin",
-//            Role = BilQalaam.Domain.Enums.UserRole.SuperAdmin,
-//            EmailConfirmed = true,
-//            PhoneNumber = "0000000000"
-//        };
+    var superAdminUser = await userManager.FindByEmailAsync(superAdminEmail);
+    if (superAdminUser == null)
+    {
+        superAdminUser = new ApplicationUser
+        {
+            UserName = superAdminEmail,
+            Email = superAdminEmail,
+            FullName = "Super Admin",
+            Role = BilQalaam.Domain.Enums.UserRole.SuperAdmin,
+            EmailConfirmed = true,
+            PhoneNumber = "0000000000"
+        };
 
-//        var result = await userManager.CreateAsync(superAdminUser, superAdminPassword);
-//        if (result.Succeeded)
-//        {
-//            await userManager.AddToRoleAsync(superAdminUser, superAdminRole);
-//        }
-//    }
-//}
-app.MapGet("/", () => "BilQalaam API is running 🚀");
+        var result = await userManager.CreateAsync(superAdminUser, superAdminPassword);
+        if (result.Succeeded)
+        {
+            await userManager.AddToRoleAsync(superAdminUser, superAdminRole);
+        }
+    }
+}
 
 // =====================
 // Middleware
 // =====================
-
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
+}
 
+app.UseHttpsRedirection();
 
 // 🔥 CORS لازم قبل Auth
 app.UseCors("AllowAngular");
